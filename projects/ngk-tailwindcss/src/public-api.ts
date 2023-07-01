1d42b590f795eddb1c31ab4ddb50334efe8d1663
Prasanna Raja <prasannaraja@msn.com>
2023-07-01 09:13:15 +0200

    Update public-api.ts

--- projects/ngk-tailwindcss/src/public-api.ts
+++ projects/ngk-tailwindcss/src/public-api.ts
@@ -2,6 +2,15 @@
  * Public API Surface of ngk-tailwindcss
  */
 
+export * from './lib/components/auto-complete/auto-complete.component';
+export * from './lib/components/button/button.component';
+export * from './lib/components/calendar/calendar.component';
+export * from './lib/components/checkbox/checkbox.component';
+export * from './lib/components/checkbox-group/checkbox-group.component';
+export * from './lib/components/comboboxes/comboboxes.component';
+export * from './lib/components/dropdown/dropdown.component';
+export * from './lib/components/input-field/input-field.component';
+export * from './lib/components/table/table.component';
+export * from './lib/components/text-field/text-field.component';
 export * from './lib/ngk-tailwindcss.service';
-export * from './lib/ngk-tailwindcss.component';
 export * from './lib/ngk-tailwindcss.module';
