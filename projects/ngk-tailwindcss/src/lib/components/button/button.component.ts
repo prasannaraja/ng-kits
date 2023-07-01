d5dbdb55aea1d64c875cd0815eb3a35b52db5260
Prasanna Raja <prasannaraja@msn.com>
2023-07-01 09:10:38 +0200

    button component added

---
+++ projects/ngk-tailwindcss/src/lib/components/button/button.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'tailwind-button',
+  templateUrl: './button.component.html',
+  styleUrls: ['./button.component.css']
+})
+export class ButtonComponent {
+
+}
