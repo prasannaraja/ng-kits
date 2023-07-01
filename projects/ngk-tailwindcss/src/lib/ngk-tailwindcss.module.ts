d0882b69ca7f114418b3397820d98688d3cb96ec
Prasanna Raja <prasannaraja@msn.com>
2023-07-01 09:13:10 +0200

    Update ngk-tailwindcss.module.ts

--- projects/ngk-tailwindcss/src/lib/ngk-tailwindcss.module.ts
+++ projects/ngk-tailwindcss/src/lib/ngk-tailwindcss.module.ts
@@ -1,16 +1,38 @@
 import { NgModule } from '@angular/core';
-import { NgkTailwindcssComponent } from './ngk-tailwindcss.component';
-
-
+import { NgkTailwindcssComponent } from './components/default/ngk-tailwindcss.component';
+import { AutoCompleteComponent } from './components/auto-complete/auto-complete.component';
+import { ButtonComponent } from './components/button/button.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
+import { CheckboxComponent } from './components/checkbox/checkbox.component';
+import { CheckboxGroupComponent } from './components/checkbox-group/checkbox-group.component';
+import { ComboboxesComponent } from './components/comboboxes/comboboxes.component';
+import { InputFieldComponent } from './components/input-field/input-field.component';
+import { TableComponent } from './components/table/table.component';
+import { TextFieldComponent } from './components/text-field/text-field.component';
 
 @NgModule({
   declarations: [
-    NgkTailwindcssComponent
+    AutoCompleteComponent,
+    ButtonComponent,
+    CalendarComponent,
+    CheckboxComponent,
+    CheckboxGroupComponent,
+    ComboboxesComponent,
+    InputFieldComponent,
+    TableComponent,
+    TextFieldComponent,
   ],
-  imports: [
-  ],
+  imports: [],
   exports: [
-    NgkTailwindcssComponent
-  ]
+    AutoCompleteComponent,
+    ButtonComponent,
+    CalendarComponent,
+    CheckboxComponent,
+    CheckboxGroupComponent,
+    ComboboxesComponent,
+    InputFieldComponent,
+    TableComponent,
+    TextFieldComponent,
+  ],
 })
-export class NgkTailwindcssModule { }
+export class NgkTailwindcssModule {}
