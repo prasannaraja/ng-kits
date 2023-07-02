f52703f45f811b2a37e87fc6f2b5268056920139
Prasanna Raja <prasannaraja@msn.com>
2023-07-02 23:30:01 +0200

    Update public-api.ts

--- projects/ngk-composite-builder/src/public-api.ts
+++ projects/ngk-composite-builder/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngk-composite-builder
  */
 
-export * from './lib/ngk-composite-builder.service';
-export * from './lib/ngk-composite-builder.component';
+export * from './lib/services/ngk-composite-builder.service';
+export * from './lib/components/ngk-composite-builder.component';
 export * from './lib/ngk-composite-builder.module';
