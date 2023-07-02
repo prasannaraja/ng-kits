81afb17bee45da05b499b0199946b169c2a30947
Prasanna Raja <prasannaraja@msn.com>
2023-07-02 21:58:12 +0200

    composite builder and example application added

---
+++ projects/ngk-composite-builder/src/lib/ngk-composite-builder.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { NgkCompositeBuilderComponent } from './ngk-composite-builder.component';
+
+
+
+@NgModule({
+  declarations: [
+    NgkCompositeBuilderComponent
+  ],
+  imports: [
+  ],
+  exports: [
+    NgkCompositeBuilderComponent
+  ]
+})
+export class NgkCompositeBuilderModule { }
