fa722e06ccad8b0f51ad6b2f472c85e04f990dac
Prasanna Raja <prasannaraja@msn.com>
2023-07-02 23:30:32 +0200

    Create ngk-composite-builder.component.ts

---
+++ projects/ngk-composite-builder/src/lib/components/ngk-composite-builder.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-ngk-composite-builder',
+  template: `
+    <p>
+      ngk-composite-builder works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class NgkCompositeBuilderComponent {
+
+}
